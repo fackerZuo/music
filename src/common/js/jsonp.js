888d82120a8e009763d3983a46e88a1be4c95a54
DESKTOP-3Q5KFUB\Lenovo <fackerzuo>
2017-08-01 22:29:14 +0800

    获取到QQ音乐的banner轮播图地址和跳转地址

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -2,20 +2,20 @@ import originJSONP from 'jsonp'
 
 export default function jsonp(url, data, option) {
   url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
-  return new Promise((resolve, rejecct) => {
+  return new Promise((resolve, reject) => {
     originJSONP(url, option, (err, data) => {
       if (!err) {
         resolve(data)
       } else {
-        rejecct(err)
+        reject(err)
       }
     })
   })
 }
 
-function param(data){
+function param (data) {
   let url = ''
-  for (var k in data){
+  for (var k in data) {
     let value = data[k] !== undefined ? data[k] : ''
     url += `&${k}=${encodeURIComponent(value)}`
   }
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -2,6 +2,11 @@
   <div class="recommend" >
     <div class="recommend-content">
       <div class="slider-wrapper" >
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl"><img :src="item.picUrl" alt=""></a>
+          </div>
+        </slider>
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
@@ -14,6 +19,31 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import Slider from 'base/slider/slider'
+  import {getRecommend} from 'api/recommend.js'
+  import {ERR_OK} from 'api/config'
+  export default {
+    data() {
+      return {
+        recommends: []
+      }
+    },
+    created() {
+      this._getRecommend()
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
+          }
+        })
+      }
+    },
+    components: {
+      Slider
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
