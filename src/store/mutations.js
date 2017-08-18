df2c3fd83a565b5c589e354e3bf5a2a3057985ca
fackerzuo <562086977@qq.com>
2017-08-18 18:39:21 +0800

    使用vuex建立音乐播放的最基本数据

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,6 +2,24 @@ import * as types from './mutation-types'
 const matutions = {
   [types.SET_SINGER](state, singer) {
     state.singer = singer
+  },
+  [types.SET_PLAY_STATE](state, flag) {
+    state.playing = flag
+  },
+  [types.SET_FULL_SCREEN](state, flag) {
+    state.fullScreen = flag
+  },
+  [types.SET_PLAY_LIST](state, list) {
+    state.playList = list
+  },
+  [types.SET_SEQUENCE_LIST](state, list) {
+    state.sequenceList = list
+  },
+  [types.SET_MODE](state, mode) {
+    state.mode = mode
+  },
+  [types.SET_CURRENTINDEX](state, index) {
+    state.currentIndex = index
   }
 }
 export default matutions
